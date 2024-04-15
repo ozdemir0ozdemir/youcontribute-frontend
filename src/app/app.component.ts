b43e20771a110d37df05a7be3c309280eba94339
ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-04-15 19:34:10 +0300

    Bolum 7 - Bitis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {OneSignal} from "onesignal-ngx";
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +8,12 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 	title = 'You Contribute';
+
+	constructor(private oneSignal: OneSignal) {
+		this.oneSignal.init({
+			appId: "b4a87e78-7a84-4fc8-b06c-8cd46f9ba1be"
+		}).then(() => {
+			console.log("Abone Olundu!")
+		});
+	}
 }
